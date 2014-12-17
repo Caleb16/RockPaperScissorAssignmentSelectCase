938d0b3de3de5d956589a44f74542062a9bbf237
Caleb16 <crank1@swceschool.org>
2014-12-17 13:34:16 -0700

    Added Files

---
+++ Select-Case-Assignment/Form1.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblInstructions = New System.Windows.Forms.Label()
+        Me.rdoRock = New System.Windows.Forms.RadioButton()
+        Me.rdoPaper = New System.Windows.Forms.RadioButton()
+        Me.rdoScissors = New System.Windows.Forms.RadioButton()
+        Me.btnThrow = New System.Windows.Forms.Button()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lblInstructions
+        '
+        Me.lblInstructions.AutoSize = True
+        Me.lblInstructions.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblInstructions.Location = New System.Drawing.Point(47, 126)
+        Me.lblInstructions.Name = "lblInstructions"
+        Me.lblInstructions.Size = New System.Drawing.Size(166, 20)
+        Me.lblInstructions.TabIndex = 0
+        Me.lblInstructions.Text = "Choose Your Throw"
+        '
+        'rdoRock
+        '
+        Me.rdoRock.AutoSize = True
+        Me.rdoRock.Location = New System.Drawing.Point(28, 169)
+        Me.rdoRock.Name = "rdoRock"
+        Me.rdoRock.Size = New System.Drawing.Size(51, 17)
+        Me.rdoRock.TabIndex = 2
+        Me.rdoRock.TabStop = True
+        Me.rdoRock.Text = "Rock"
+        Me.rdoRock.UseVisualStyleBackColor = True
+        '
+        'rdoPaper
+        '
+        Me.rdoPaper.AutoSize = True
+        Me.rdoPaper.Location = New System.Drawing.Point(28, 206)
+        Me.rdoPaper.Name = "rdoPaper"
+        Me.rdoPaper.Size = New System.Drawing.Size(53, 17)
+        Me.rdoPaper.TabIndex = 3
+        Me.rdoPaper.TabStop = True
+        Me.rdoPaper.Text = "Paper"
+        Me.rdoPaper.UseVisualStyleBackColor = True
+        '
+        'rdoScissors
+        '
+        Me.rdoScissors.AutoSize = True
+        Me.rdoScissors.Location = New System.Drawing.Point(28, 243)
+        Me.rdoScissors.Name = "rdoScissors"
+        Me.rdoScissors.Size = New System.Drawing.Size(64, 17)
+        Me.rdoScissors.TabIndex = 4
+        Me.rdoScissors.TabStop = True
+        Me.rdoScissors.Text = "Scissors"
+        Me.rdoScissors.UseVisualStyleBackColor = True
+        '
+        'btnThrow
+        '
+        Me.btnThrow.Location = New System.Drawing.Point(91, 281)
+        Me.btnThrow.Name = "btnThrow"
+        Me.btnThrow.Size = New System.Drawing.Size(75, 23)
+        Me.btnThrow.TabIndex = 5
+        Me.btnThrow.Text = "Throw"
+        Me.btnThrow.UseVisualStyleBackColor = True
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.5!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResult.Location = New System.Drawing.Point(15, 310)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(0, 16)
+        Me.lblResult.TabIndex = 6
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(14, 29)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(247, 20)
+        Me.lblName.TabIndex = 7
+        Me.lblName.Text = "Enter Your First Name Please"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(81, 76)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(100, 20)
+        Me.txtName.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(282, 360)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.btnThrow)
+        Me.Controls.Add(Me.rdoScissors)
+        Me.Controls.Add(Me.rdoPaper)
+        Me.Controls.Add(Me.rdoRock)
+        Me.Controls.Add(Me.lblInstructions)
+        Me.Name = "Form1"
+        Me.Text = "Rock Paper Scissors Game"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblInstructions As System.Windows.Forms.Label
+    Friend WithEvents rdoRock As System.Windows.Forms.RadioButton
+    Friend WithEvents rdoPaper As System.Windows.Forms.RadioButton
+    Friend WithEvents rdoScissors As System.Windows.Forms.RadioButton
+    Friend WithEvents btnThrow As System.Windows.Forms.Button
+    Friend WithEvents lblResult As System.Windows.Forms.Label
+    Friend WithEvents lblName As System.Windows.Forms.Label
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
+
+End Class
---
+++ Select-Case-Assignment/Form1.vb
@@ -0,0 +1,71 @@
+﻿Public Class Form1
+    
+    Private Sub btnThrow_Click(sender As Object, e As EventArgs) Handles btnThrow.Click
+        Dim intRock As Integer = 1
+        Dim intPaper As Integer = 2
+        Dim intScissors As Integer = 3
+        Dim intComThrow As Integer
+        Dim strName As String = Me.txtName.Text
+
+        Randomize()
+        intComThrow = Int((3 - 1 + 1) * Rnd() + 1)
+
+        Select Case intComThrow
+            Case intRock
+                Select Case Me.rdoRock.Checked
+                    Case True
+                        Me.lblResult.Text = "Rock Vs. Rock, Tie"
+                    Case False
+                        'Do Nothing
+                End Select
+                Select Case Me.rdoPaper.Checked
+                    Case True
+                        Me.lblResult.Text = "Paper Covers Rock, " & strName & " Wins"
+                    Case False
+                        'Do Nothing
+                End Select
+                Select Case Me.rdoScissors.Checked
+                    Case True
+                        Me.lblResult.Text = "Rock Smashes Scissors, Computer Wins"
+                End Select
+            Case intPaper
+                Select Case Me.rdoRock.Checked
+                    Case True
+                        Me.lblResult.Text = "Paper Covers Rock, Computer Wins"
+                    Case False
+                        'Do Nothing
+                End Select
+                Select Case Me.rdoPaper.Checked
+                    Case True
+                        Me.lblResult.Text = "Paper Vs. Paper, Tie"
+                    Case False
+                        'Do Nothing
+                End Select
+                Select Case Me.rdoScissors.Checked
+                    Case True
+                        Me.lblResult.Text = "Scissors Cut Paper, " & strName & " Wins"
+                    Case False
+                        'Do Nothing
+                End Select
+            Case intScissors
+                Select Case Me.rdoRock.Checked
+                    Case True
+                        Me.lblResult.Text = "Rock Smashes Scissors, " & strName & " Wins"
+                    Case False
+                        'Do Nothing
+                End Select
+                Select Case Me.rdoPaper.Checked
+                    Case True
+                        Me.lblResult.Text = "Scissors Cut Paper, Computer Wins"
+                    Case False
+                        'Do Nothing
+                End Select
+                Select Case Me.rdoScissors.Checked
+                    Case True
+                        Me.lblResult.Text = "Scissors Vs. Scissors, Tie"
+                    Case False
+                        'Do Nothing
+                End Select
+        End Select
+    End Sub
+End Class
